bd90b5068f98f83a4067f19f629caf8329458c77
Adrian Thomas <thomasa8992@hotmail.com>
2020-02-23 15:25:52 -0600

    added logic to detect items, refactored code previously in the Update function

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -18,41 +19,72 @@ public class MouseManager : MonoBehaviour
     private int LeftMouseButton = 0;
     // Update is called once per frame
     void Update() {
+        handleDetectionOfGameObjects();
+    }
+
+    private void handleDetectionOfGameObjects() {
         RaycastHit hit;
-        bool isDoor = false;
 
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, ClickableLayer.value)) {
-            isDoor = doorwayIsDectected(hit, isDoor);
-        } else {
-            Cursor.SetCursor(NormalPointer, Vector2.zero, CursorMode.Auto);
-        }
+            var isDoor = false;
+            var isItem = false;
+            var isEnemy = false;
 
-        detectLeftMouseButtonClick(hit, isDoor);
+            if (hit.collider.gameObject.tag == "Doorway") {
+                isDoor = doorwayIsDectected(hit, isDoor);
+            } else if (hit.collider.gameObject.tag == "Item") {
+                isItem = itemIsDectected(hit, isItem);
+            } else {
+                Cursor.SetCursor(ClickableObjectCursor, Vector2.zero, CursorMode.Auto);
+            }
+
+            detectLeftMouseButtonClick(hit, isDoor, isItem);
+        }
     }
 
     private bool doorwayIsDectected(RaycastHit hit, bool isDoor) {
-        if (hit.collider.gameObject.tag == "Doorway") {
-            Cursor.SetCursor(DoorwayCursor, new Vector2(16, 16), CursorMode.Auto);
-            isDoor = true;
-        } else {
-            Cursor.SetCursor(ClickableObjectCursor, new Vector2(16, 16), CursorMode.Auto);
-        }
-
-        return isDoor;
+        Cursor.SetCursor(DoorwayCursor, new Vector2(16, 16), CursorMode.Auto);
+        return true;
     }
 
-    private void detectLeftMouseButtonClick(RaycastHit hit, bool isDoor) {
+    private bool itemIsDectected(RaycastHit hit, bool isItem) {
+        Cursor.SetCursor(CombatCursor, new Vector2(16, 16), CursorMode.Auto);
+        return true;
+    }
+
+    private void detectLeftMouseButtonClick(RaycastHit hit, bool isDoor, bool isItem) {
         if (Input.GetMouseButtonDown(LeftMouseButton)) {
             if(isDoor) {
-                Transform doorway = hit.collider.gameObject.transform;
-                OnClickEnvironment.Invoke(doorway.position);
-
-                Debug.Log("Door");
-            }
-            else {
+                handleDoorAction(hit, isDoor);
+            } 
+            else if(isItem) {
+                handleItemAction(hit, isItem);
+            } else {
                 OnClickEnvironment.Invoke(hit.point);
             }
         }
+    }
+
+    private void handleItemAction(RaycastHit hit, bool isItem) {
+        moveToItem(hit);
+    }
+
+    private void handleDoorAction(RaycastHit hit, bool isDoor) {
+        moveToDoor(hit);
+    }
+
+    private void moveToDoor(RaycastHit hit) {
+        Transform doorway = hit.collider.gameObject.transform;
+
+        OnClickEnvironment.Invoke(doorway.position);
+        Debug.Log("Door");
+    }
+
+    private void moveToItem(RaycastHit hit) {
+        Transform item = hit.collider.gameObject.transform;
+
+        OnClickEnvironment.Invoke(item.position);
+        Debug.Log("Item");
     }
 
 
