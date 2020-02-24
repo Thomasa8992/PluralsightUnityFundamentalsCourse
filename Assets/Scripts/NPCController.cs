410e8b195f0dec02a079c8c92dcb7fb32c65201e
Adrian Thomas <thomasa8992@hotmail.com>
2020-02-23 22:11:59 -0600

    added npc movement and chase logic

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour
+{
+    public float PatrolTime = 10f;
+    public float AggroRange = 10;
+    public Transform[] Waypoints;
+
+    private int index;
+    private float speed;
+    private float agentSpeed;
+    private Transform player;
+
+    Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake() {
+        //animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        if(navMeshAgent != null) {
+            agentSpeed = navMeshAgent.speed;
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, Waypoints.Length);
+
+        InvokeRepeating("tick", 0, 0.5f);
+
+        if(Waypoints.Length > 0) {
+            InvokeRepeating("patrol", 0, PatrolTime);
+        }
+    }
+
+    private void patrol() {
+        index = index == Waypoints.Length - 1 ? 0 : index + 1; 
+    }
+
+    private void tick() {
+        navMeshAgent.destination = Waypoints[index].position;
+        navMeshAgent.speed = agentSpeed / 2;
+
+        if (player != null && Vector3.Distance(transform.position, player.position) < AggroRange) {
+            navMeshAgent.destination = player.position;
+            navMeshAgent.speed = agentSpeed;
+        }
+    }
+
+}
